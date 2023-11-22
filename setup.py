9ffff92c4277ce44589d8f8afc258987e5b4cedc
Dannvrg12 <142259195+Dannvrg12@users.noreply.github.com>
2023-11-22 15:51:33 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='N Reinas',
+      version='0.1',
+      description='A Python package for something',
+      url='https://github.com/Dannvrg12/N-Reinas',
+      author='Daniel Vargas',
+      author_email='va401684@uaeh.edu.mx',
+      license='MIT',
+      packages=['N-Reinas'],
+      zip_safe=False)
